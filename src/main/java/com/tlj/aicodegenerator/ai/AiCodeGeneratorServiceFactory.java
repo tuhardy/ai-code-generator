5b29a97f14fb12d5cce6df5d400968b36f3220b7
tulj <3215267997@qq.com>
2025-09-25 20:23:07 +0800

    完成应用模块基本功能，人工测试通过

--- src/main/java/com/tlj/aicodegenerator/ai/AiCodeGeneratorServiceFactory.java
+++ src/main/java/com/tlj/aicodegenerator/ai/AiCodeGeneratorServiceFactory.java
@@ -1,6 +1,7 @@
 package com.tlj.aicodegenerator.ai;
 
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.service.AiServices;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +11,14 @@ import org.springframework.context.annotation.Configuration;
 public class AiCodeGeneratorServiceFactory {
     @Resource
     private ChatModel chatModel;
-
+    @Resource
+    private StreamingChatModel streamingChatModel;
 
     @Bean
     public AiCodeGeneratorService aiCodeGeneratorService(){
-        return AiServices.create( AiCodeGeneratorService.class,chatModel);
+        return AiServices.builder( AiCodeGeneratorService.class)
+                .chatModel(chatModel)
+                .streamingChatModel(streamingChatModel)
+                .build();
     }
 }
